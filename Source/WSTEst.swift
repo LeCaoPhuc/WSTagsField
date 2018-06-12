49b350a663cd4dee5bfdb3d50551f15ad9961a84
Phuc Le <phucle>
2018-06-12 11:29:25 +0700

    add test file

---
+++ Source/WSTEst.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  WSTagsField
+//
+//  Created by Cao Phuc on 6/12/18.
+//  Copyright © 2018 Whitesmith. All rights reserved.
+//
+
+import Foundation
+open class WSTest {
+    open func getTest() -> String {
+        return "Test Strimng";
+    }
+}
